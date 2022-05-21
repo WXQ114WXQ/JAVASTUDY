e462e753860c88428bfde76395a7d1e28a1a7797
WXQ <1067782756@qq.com>
2022-05-21 16:01:51 +0800

    demo-eureka-provider 提交到仓库

--- demo-eureka-auth_server/src/main/java/com/qdc/demoeurekaauth_server/DemoEurekaAuthServerApplication.java
+++ demo-eureka-auth_server/src/main/java/com/qdc/demoeurekaauth_server/DemoEurekaAuthServerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 public class DemoEurekaAuthServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("提交到GitHub");
 		SpringApplication.run(DemoEurekaAuthServerApplication.class, args);
 	}
 
